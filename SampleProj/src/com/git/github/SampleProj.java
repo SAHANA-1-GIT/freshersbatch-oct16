19a37e683a53889cd721ab047bf99761de15ffc0
SAHANA-1-GIT <68469539+SAHANA-1-GIT@users.noreply.github.com>
2021-04-10 19:02:05 +0530

    first commit

---
+++ SampleProj/src/com/git/github/SampleProj.java
@@ -0,0 +1,12 @@
+package com.git.github;
+
+public class SampleProj {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("welcome");
+
+	}
+
+}
